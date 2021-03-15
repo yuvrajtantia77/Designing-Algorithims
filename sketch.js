046e33494b827e11d8abffbd82a7926ac3051d18
yuvrajtantia77 <79694233+yuvrajtantia77@users.noreply.github.com>
2021-03-15 20:06:27 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixedRectangle;
+var movingRectangle;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRectangle=createSprite(600, 400, 50, 80);
+  fixedRectangle.shapeColor="green"
+  movingRectangle=createSprite(400, 200, 80, 30);
+  movingRectangle.shapeColor="green"
+}
+
+function draw() {
+  background("black");  
+  movingRectangle.x=World.mouseX;
+  movingRectangle.y=World.mouseY;
+  if(movingRectangle.x-fixedRectangle.x<fixedRectangle.width/2+movingRectangle.width/2
+    &&fixedRectangle.x-movingRectangle.x<fixedRectangle.width/2+movingRectangle.width/2
+    &&movingRectangle.y-fixedRectangle.y<fixedRectangle.width/2+movingRectangle.width/2
+    &&fixedRectangle.y-movingRectangle.y<fixedRectangle.width/2+movingRectangle.width/2){
+    movingRectangle.shapeColor="red"
+    fixedRectangle.shapeColor="red"
+  }
+  else{
+    movingRectangle.shapeColor="yellow"
+    fixedRectangle.shapeColor="yellow"
+  }
+  drawSprites();
+}
